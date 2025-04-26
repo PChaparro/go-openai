932422781b0d9e555b85aa5c845b5020ed118657
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2025-04-26 08:38:11 -0500

    refactor: rename fork for personal usage

--- files_api_test.go
+++ files_api_test.go
@@ -12,8 +12,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/pchaparro/go-openai/internal/test/checks"
 	"github.com/sashabaranov/go-openai"
-	"github.com/sashabaranov/go-openai/internal/test/checks"
 )
 
 func TestFileBytesUpload(t *testing.T) {
--- stream_reader_test.go
+++ stream_reader_test.go
@@ -6,9 +6,9 @@ import (
 	"errors"
 	"testing"
 
-	utils "github.com/sashabaranov/go-openai/internal"
-	"github.com/sashabaranov/go-openai/internal/test"
-	"github.com/sashabaranov/go-openai/internal/test/checks"
+	utils "github.com/pchaparro/go-openai/internal"
+	"github.com/pchaparro/go-openai/internal/test"
+	"github.com/pchaparro/go-openai/internal/test/checks"
 )
 
 var errTestUnmarshalerFailed = errors.New("test unmarshaler failed")
